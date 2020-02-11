e2656a2987f602faaacc1e2f0922673b0e51bedf
deepaknalore <deepak.s.nalore@gmail.com>
2020-02-11 03:09:13 -0600

    Working

---
+++ HW1/Q7_Plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+#####           MAIN        #########
+nodes = [6, 12, 28, 59, 107]
+error = [315, 178, 104, 56, 32]
+n = ["d32", "d128", "d512", "d2048", "d8192"]
+
+plt.scatter(nodes, error)
+plt.title("Err_n v/s Number of nodes")
+plt.xlabel("Number of nodes")
+plt.ylabel("Number of Errors")
+
+for i, txt in enumerate(n):
+    plt.annotate(txt, (nodes[i], error[i]))
+plt.show()
